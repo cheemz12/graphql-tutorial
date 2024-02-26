c3ab8f2b109825bee77d11a366f2e46b8afb8ef3
Zayed Cheema <zayed@lawfty.com>
2024-02-26 18:14:07 -0500

    added paginate search

--- app/graphql/mutations/create_link.rb
+++ app/graphql/mutations/create_link.rb
@@ -1,16 +1,18 @@
 module Mutations
-    class CreateLink < BaseMutation
-      argument :description, String, required: true
-      argument :url, String, required: true
+  class CreateLink < BaseMutation
+    argument :description, String, required: true
+    argument :url, String, required: true
 
-      type Types::LinkType
+    type Types::LinkType
 
-      def resolve(description: nil, url: nil)
-        Link.create!(
-          description: description,
-          url: url,
-          user: context[:current_user]
-        )
-      end
+    def resolve(description: nil, url: nil)
+      Link.create!(
+        description: description,
+        url: url,
+        user: context[:current_user]
+      )
+    rescue ActiveRecord::RecordInvalid => e
+      GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
     end
   end
+end
